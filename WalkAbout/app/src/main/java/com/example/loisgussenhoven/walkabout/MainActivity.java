e03aeff5f8d762f0a7e70165a55a4fca023f7d2f
Unknown <lois_gussenhoven@hotmail.nl>
2017-12-11 11:32:53 +0100

    First try at lay-outs for main, map and info

---
+++ WalkAbout/app/src/main/java/com/example/loisgussenhoven/walkabout/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.loisgussenhoven.walkabout;
+
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class MainActivity extends AppCompatActivity {
+
+         @Override
+        protected void onCreate (Bundle savedInstanceState){
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+
+            //Language Spinner
+                 List spinnerArray =  new ArrayList<String>();
+                 spinnerArray.add("Nederlands");
+                 spinnerArray.add("English");
+
+                 ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                         this, android.R.layout.simple_spinner_item, spinnerArray);
+
+                 adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                 Spinner sItems = findViewById(R.id.spinner);
+                 sItems.setAdapter(adapter);
+
+                 String selected = sItems.getSelectedItem().toString();
+                 if (selected.equals("English")) {
+                     //TODO: change app language
+             }
+             //Language Spinner
+             }
+        }
+
+
+
+
