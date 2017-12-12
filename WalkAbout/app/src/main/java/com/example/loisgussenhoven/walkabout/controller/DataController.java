04f1a82d4162a167d49febf54effff363cada4bb
Jaap-Jan de Wit <jipjan@gmail.com>
2017-12-12 11:42:39 +0100

    start of implementation datacontroller

--- WalkAbout/app/src/main/java/com/example/loisgussenhoven/walkabout/controller/DataController.java
+++ WalkAbout/app/src/main/java/com/example/loisgussenhoven/walkabout/controller/DataController.java
@@ -1,8 +1,49 @@
 package com.example.loisgussenhoven.walkabout.controller;
 
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import com.example.loisgussenhoven.walkabout.model.Pinpoint;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 /**
  * Created by Jaap-Jan on 12-12-2017.
  */
 
-public class DataController {
+public class DataController extends SQLiteOpenHelper {
+
+    private List<Pinpoint> session;
+    private HashMap<String, String> routes;
+
+
+    public DataController() {
+
+    }
+
+
+    public String getRouteInformation(String name) {
+        return routes.get(name);
+    }
+
+    public void setNewSession() {
+        session = new ArrayList<Pinpoint>();
+    }
+
+    public void addPinpointToSession(Pinpoint p) {
+        session.add(p);
+    }
+
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
 }
