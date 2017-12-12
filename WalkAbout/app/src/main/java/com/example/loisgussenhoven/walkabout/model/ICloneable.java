5119f6353a46452a2f24e45b875f9c5ac10abb87
Jaap-Jan de Wit <jipjan@gmail.com>
2017-12-12 11:35:45 +0100

    implementation of cloneable and equals/hash

---
+++ WalkAbout/app/src/main/java/com/example/loisgussenhoven/walkabout/model/ICloneable.java
@@ -0,0 +1,9 @@
+package com.example.loisgussenhoven.walkabout.model;
+
+/**
+ * Created by Jaap-Jan on 12-12-2017.
+ */
+
+public interface ICloneable<T> {
+    T clone();
+}
--- WalkAbout/app/src/main/java/com/example/loisgussenhoven/walkabout/model/Pinpoint.java
+++ WalkAbout/app/src/main/java/com/example/loisgussenhoven/walkabout/model/Pinpoint.java
@@ -6,7 +6,7 @@ import java.util.List;
  * Created by Jaap-Jan on 12-12-2017.
  */
 
-public class Pinpoint {
+public class Pinpoint implements ICloneable<Pinpoint> {
     private String name, information, author;
     private int year;
     private List<Integer> images;
@@ -58,5 +58,42 @@ public class Pinpoint {
 
     public void setHasBeenVisitedOrSkipped(boolean hasBeenVisitedOrSkipped) {
         this.hasBeenVisitedOrSkipped = hasBeenVisitedOrSkipped;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Pinpoint pinpoint = (Pinpoint) o;
+
+        if (year != pinpoint.year) return false;
+        if (longitude != pinpoint.longitude) return false;
+        if (latitude != pinpoint.latitude) return false;
+        if (hasBeenVisitedOrSkipped != pinpoint.hasBeenVisitedOrSkipped) return false;
+        if (name != null ? !name.equals(pinpoint.name) : pinpoint.name != null) return false;
+        if (information != null ? !information.equals(pinpoint.information) : pinpoint.information != null)
+            return false;
+        if (author != null ? !author.equals(pinpoint.author) : pinpoint.author != null)
+            return false;
+        return images != null ? images.equals(pinpoint.images) : pinpoint.images == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name != null ? name.hashCode() : 0;
+        result = 31 * result + (information != null ? information.hashCode() : 0);
+        result = 31 * result + (author != null ? author.hashCode() : 0);
+        result = 31 * result + year;
+        result = 31 * result + (images != null ? images.hashCode() : 0);
+        result = 31 * result + (int) (longitude ^ (longitude >>> 32));
+        result = 31 * result + (int) (latitude ^ (latitude >>> 32));
+        result = 31 * result + (hasBeenVisitedOrSkipped ? 1 : 0);
+        return result;
+    }
+
+    @Override
+    public Pinpoint clone() {
+        return new Pinpoint(name, information, author, year, images, longitude, latitude, hasBeenVisitedOrSkipped);
     }
 }
