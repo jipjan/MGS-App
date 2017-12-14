940ffcd541a30c44635aa6617b832b3302b7056f
Jaap-Jan de Wit <jipjan@gmail.com>
2017-12-14 19:46:03 +0100

    GsonResponse classs so we can make volley requests that return a gson json class

--- WalkAbout/app/src/main/java/com/example/loisgussenhoven/walkabout/controller/RouteController.java
+++ WalkAbout/app/src/main/java/com/example/loisgussenhoven/walkabout/controller/RouteController.java
@@ -1,5 +1,12 @@
 package com.example.loisgussenhoven.walkabout.controller;
 
+import android.content.Context;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+import com.example.loisgussenhoven.walkabout.controller.json.Directions;
 import com.google.android.gms.maps.model.LatLng;
 
 /**
@@ -11,8 +18,15 @@ public class RouteController {
     private static final String BASE_DIRECTION_URL = "https://maps.googleapis.com/maps/api/directions/json?";
     private static final String API_KEY = "AIzaSyA5kN2wxpBXQQ55yVjUdItOAdGFO9dZZGY";
 
-    public static void getDirections(LatLng start, LatLng dest) {
+    private RequestQueue queue;
+
+    public RouteController(Context context) {
+        queue = Volley.newRequestQueue(context);
+    }
+
+    public void getDirections(LatLng start, LatLng dest, Response.Listener<Directions> onSucces, Response.ErrorListener onFail) {
         String requestUrl = BASE_DIRECTION_URL + "origin=" + start.latitude + "," + start.longitude + "&destination=" + dest.latitude + "," + dest.longitude + "&key=" + API_KEY;
-        ;
+        GsonRequest<Directions> request = new GsonRequest<>(Request.Method.GET, requestUrl, Directions.class, onSucces, onFail);
+        queue.add(request);
     }
 }
