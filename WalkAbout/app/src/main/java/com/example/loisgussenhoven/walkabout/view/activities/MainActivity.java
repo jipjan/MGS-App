f5e2fff935da7989879a8e9b3103698f85d8030f
Jaap-Jan de Wit <jipjan@gmail.com>
2017-12-14 14:01:20 +0100

    maps + api + permissions

--- WalkAbout/app/src/main/java/com/example/loisgussenhoven/walkabout/view/activities/MainActivity.java
+++ WalkAbout/app/src/main/java/com/example/loisgussenhoven/walkabout/view/activities/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.loisgussenhoven.walkabout.view.activities;
 
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 
 import com.example.loisgussenhoven.walkabout.R;
@@ -15,32 +18,41 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-         @Override
-        protected void onCreate (Bundle savedInstanceState){
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-             DataController test = new DataController(this);
+        DataController test = new DataController(this);
 
-            //Language Spinner
-                 List spinnerArray =  new ArrayList<String>();
-                 spinnerArray.add("Nederlands");
-                 spinnerArray.add("English");
+        //Language Spinner
+        List spinnerArray = new ArrayList<String>();
+        spinnerArray.add("Nederlands");
+        spinnerArray.add("English");
 
-                 ArrayAdapter<String> adapter = new ArrayAdapter<String>(
-                         this, android.R.layout.simple_spinner_item, spinnerArray);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                this, android.R.layout.simple_spinner_item, spinnerArray);
 
-                 adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-                 Spinner sItems = findViewById(R.id.spinner);
-                 sItems.setAdapter(adapter);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        Spinner sItems = findViewById(R.id.spinner);
+        sItems.setAdapter(adapter);
 
-                 String selected = sItems.getSelectedItem().toString();
-                 if (selected.equals("English")) {
-                     //TODO: change app language
-             }
-             //Language Spinner
-             }
+        String selected = sItems.getSelectedItem().toString();
+        if (selected.equals("English")) {
+            //TODO: change app language
         }
+        //Language Spinner
+
+        Button startRoute = findViewById(R.id.AM_BTN_Start);
+        startRoute.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent t = new Intent(MainActivity.this, MapActivity.class);
+                startActivity(t);
+            }
+        });
+    }
+}
 
 
 
