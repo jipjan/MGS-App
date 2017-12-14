feb4c9e612a5d56ba70c891e03c1a61cf74a1b0d
Jaap-Jan de Wit <jipjan@gmail.com>
2017-12-14 17:10:27 +0100

    ask for permissions

--- WalkAbout/app/src/main/java/com/example/loisgussenhoven/walkabout/view/activities/MapsActivity.java
+++ WalkAbout/app/src/main/java/com/example/loisgussenhoven/walkabout/view/activities/MapsActivity.java
@@ -1,6 +1,7 @@
 package com.example.loisgussenhoven.walkabout.view.activities;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
@@ -24,6 +25,8 @@ import java.util.List;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
+    private GoogleMap map;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,32 +37,40 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
     @Override
-    public void onMapReady(GoogleMap googleMap) {
-        if (ActivityCompat.checkSelfPermission(MapsActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(MapsActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            Toast toast = Toast.makeText(this, "Lacking permissions", Toast.LENGTH_LONG);
-            toast.show();
-            return;
+    public void onMapReady(final GoogleMap googleMap) {
+        map = googleMap;
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[] { Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 0);
         }
 
         googleMap.getUiSettings().setMyLocationButtonEnabled(true);
-        googleMap.setMyLocationEnabled(true);
         googleMap.getUiSettings().setZoomControlsEnabled(true);
 
+        addRoutePoints(googleMap);
+
+        googleMap.moveCamera(CameraUpdateFactory.zoomTo(17.5f));
+    }
+
+    @SuppressLint("MissingPermission")
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        if (requestCode == 0) {
+            map.setMyLocationEnabled(true);
+        }
+    }
+
+
+    private void addRoutePoints(GoogleMap googleMap) {
         List<RoutePoint> points = new DataController(this).allRoutePoints();
         for (RoutePoint p : points) {
             LatLng point = new LatLng(p.getLatitude(), p.getLongitude());
             googleMap.addMarker(new MarkerOptions().position(point).title(p.getName()));
         }
         googleMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(points.get(0).getLatitude(), points.get(0).getLongitude())));
-        googleMap.moveCamera(CameraUpdateFactory.zoomTo(17.5f));
     }
 
+    private void generateBlindWallsRoute() {
 
+    }
 }
